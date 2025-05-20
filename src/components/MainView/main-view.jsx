2d0bd2ef57ebbe2668cf8eb49c81c929bed66509
learning2programNF <nick.finn6@gmail.com>
2025-05-19 22:44:53 -0600

    Refactored PropTypes/data population code
    Added code to fulfill task 3.4 practicing PropTypes/data-population-code

--- src/components/MainView/main-view.jsx
+++ src/components/MainView/main-view.jsx
@@ -2,7 +2,7 @@
 import React from "react";
 
 // Importing the useState hook from React to manage component state
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 // Importing the MovieCard component to display individual movie previews
 import { MovieCard } from "../MovieCard/movie-card";
@@ -10,67 +10,13 @@ import { MovieCard } from "../MovieCard/movie-card";
 // Importing the MovieView component to display detailed information about a selected movie
 import { MovieView } from "../MovieView/movie-view";
 
+// Importing Prop-Types
+import PropTypes from "prop-types";
+
 // Defining and exporting the MainView component
 const MainView = () => {
   // Declaring the movies state, an array of movie objects with info like title, image, director, etc.
-  const [movies, setMovies] = useState([
-    {
-      id: 1,
-      title: "Inception",
-      image: "https://m.media-amazon.com/images/I/51zUbui+gbL._AC_.jpg",
-      director: "Christopher Nolan",
-      description:
-        "A skilled thief is given a chance at redemption if he can successfully perform inception.",
-      genre: "Science Fiction",
-      directorBio:
-        "Christopher Nolan is known for mind-bending, high-concept films like Inception, Interstellar, and Tenet.",
-    },
-    {
-      id: 2,
-      title: "The Shawshank Redemption",
-      image: "https://m.media-amazon.com/images/I/51NiGlapXlL._AC_.jpg",
-      director: "Frank Darabont",
-      description:
-        "Two imprisoned men bond over years and find redemption through acts of decency.",
-      genre: "Drama",
-      directorBio:
-        "Frank Darabont is an American director and screenwriter, known for adapting Stephen King's work.",
-    },
-    {
-      id: 3,
-      title: "The Matrix",
-      image: "https://m.media-amazon.com/images/I/51EG732BV3L.jpg",
-      director: "The Wachowskis",
-      description:
-        "A computer hacker learns the true nature of reality and his role in the war against its controllers.",
-      genre: "Action / Sci-Fi",
-      directorBio:
-        "Lana and Lilly Wachowski are filmmakers known for pushing boundaries in sci-fi with The Matrix trilogy.",
-    },
-    {
-      id: 4,
-      title: "Parasite",
-      image: "https://m.media-amazon.com/images/I/71c8kbyiG0L._AC_SY679_.jpg",
-      director: "Bong Joon-ho",
-      description:
-        "A poor family's scheme to infiltrate a wealthy household spirals out of control.",
-      genre: "Thriller / Drama",
-      directorBio:
-        "Bong Joon-ho is an Oscar-winning South Korean director known for blending social themes with dark humor.",
-    },
-    {
-      id: 5,
-      title: "The Godfather",
-      image: "https://m.media-amazon.com/images/I/51rOnIjLqzL._AC_.jpg",
-      director: "Francis Ford Coppola",
-      description:
-        "The aging patriarch of an organized crime dynasty transfers control to his reluctant son.",
-      genre: "Crime / Drama",
-      directorBio:
-        "Francis Ford Coppola is an iconic American director, best known for The Godfather trilogy and Apocalypse Now.",
-    },
-  ]);
-
+  const [movies, setMovies] = useState([]);
   // Declaring state to track which movie is currently selected (starts as null)
   const [selectedMovie, setSelectedMovie] = useState(null);
 
@@ -84,6 +30,17 @@ const MainView = () => {
     );
   }
 
+  useEffect(() => {
+    fetch("https://film-forge-11a9389fe47d.herokuapp.com/movies")
+      .then((response) => response.json())
+      .then((data) => {
+        setMovies(data);
+      })
+      .catch((error) => {
+        console.error("Error fetching movies", error);
+      });
+  }, []);
+
   // If there are no movies in the list, show a fallback message
   if (movies.length === 0) {
     return <div>The List is Empty!</div>;
@@ -94,7 +51,7 @@ const MainView = () => {
     <div>
       {movies.map((movie) => (
         <MovieCard
-          key={movie.id} // Unique key to help React track each item
+          key={movie._id} // Unique key to help React track each item
           movie={movie} // Passing movie data as a prop
           onMovieClick={(newSelectedMovie) => {
             setSelectedMovie(newSelectedMovie); // Updates the selected movie when a card is clicked
