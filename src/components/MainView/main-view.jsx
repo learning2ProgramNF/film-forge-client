2f88dc79ad491de2d1d65137dc963b98ce5011ed
learning2programNF <nick.finn6@gmail.com>
2025-06-02 18:20:40 -0600

    added LoginView/SignUpView
    login-view.jsx added
    signup-view.jsx added
    Added forms to the jsx files and restructured the MainView code to import the new signup and login views/ forms to appropriately account for the changes they brought within them and importing them into the MainView.

--- src/components/MainView/main-view.jsx
+++ src/components/MainView/main-view.jsx
@@ -10,27 +10,57 @@ import { MovieCard } from "../MovieCard/movie-card";
 // Importing the MovieView component to display detailed information about a selected movie
 import { MovieView } from "../MovieView/movie-view";
 
+// Importing the LoginView component to display the login screen
+import { LoginView } from "../LoginView/login-view"
+
+// Importing a SignUp View component to add a Sign Up Page
+import { SignUpView } from "../SignUpView/signup-view" 
+
 // Importing Prop-Types
 import PropTypes from "prop-types";
 
 // Defining and exporting the MainView component
-const MainView = () => {
+export const MainView = () => {
+  const storedUser = JSON.parse(localStorage.getItem("user"));
+  const storedToken = localStorage.getItem("token");
+  const [user, setUser] = useState(storedUser? storedUser : null);
+  const [token, setToken] = useState(storedToken? storedToken : null);
   // Declaring the movies state, an array of movie objects with info like title, image, director, etc.
   const [movies, setMovies] = useState([]);
   // Declaring state to track which movie is currently selected (starts as null)
   const [selectedMovie, setSelectedMovie] = useState(null);
 
+  if (!user) {
+    return (
+      <>
+      <LoginView
+        onLoggedIn={(user, token) => {
+          setUser(user);
+          setToken(token);
+        }} />
+        or
+        <SignUpView />
+      </>
+    );
+  }
+
   useEffect(() => {
-    fetch("https://film-forge-11a9389fe47d.herokuapp.com/movies")
+    if (!token) {
+      return;
+    }
+
+    fetch("https://film-forge-11a9389fe47d.herokuapp.com/movies", {
+      headers: { Authorization: `Bearer ${token}` }
+    })
       .then((response) => response.json())
-      .then((data) => {
-        console.log("Fetched movies", data);
-        setMovies(data);
+      .then((movies) => {
+        console.log("Fetched movies", movies);
+        setMovies(movies);
       })
       .catch((error) => {
         console.error("Error fetching movies", error);
       });
-  }, []);
+  }, [token]);
 
   // If a movie has been selected, render the MovieView component with its details
   if (selectedMovie) {
@@ -54,6 +84,7 @@ const MainView = () => {
   // If no movie is selected, show a list of MovieCard components (one per movie)
   return (
     <div>
+      <button onClick={() => { setUser(null); setToken(null); }}>Logout</button>
       {movies.map((movie) => (
         <MovieCard
           key={movie._id} // Unique key to help React track each item
